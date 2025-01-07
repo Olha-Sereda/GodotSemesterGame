a33b8f1485c82d83b272eca0fccae6a977d40228
Olha Sereda <olchik.sereda@gmail.com>
2025-01-07 21:21:37 +0100

    feat: make game manadger and its function for collactable money

---
+++ scenes/game_manadger.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var points = 0
+# Called when the node enters the scene tree for the first time.
+func add_point():
+	points +=1
+	print(points)
--- scenes/money.gd
+++ scenes/money.gd
@@ -1,7 +1,12 @@
 extends Area2D
 
+@onready var game_manager = %GameManadger
+
+
 func _on_body_entered(body: Node2D) -> void:
 	print("hello")
 	if body.is_in_group("player"):
 		queue_free()
 		print("deleted from queue")
+		game_manager.add_point()
+		print("added point")
