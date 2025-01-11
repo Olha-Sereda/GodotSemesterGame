94ecdc1a639062b61309f7620e843299f9ffec46
Olha-Sereda <olchik.sereda@gmail.com>
2025-01-11 18:42:04 +0100

    feat: add points label

--- scenes/game_manadger.gd
+++ scenes/game_manadger.gd
@@ -1,7 +1,11 @@
 extends Node
 
+@onready var points_label: Label = %PointsLabel
+
+																																																																																																																																											
 var points = 0
 # Called when the node enters the scene tree for the first time.
 func add_point():
 	points +=1
 	print(points)
+	points_label.text = "Points: " + str(points)
